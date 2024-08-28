dc30cc7dcc76fa87cb2885340c62dda232a36902
Paula Rynty <paula.rynty@gmail.com>
2024-08-28 21:00:24 +0300

    still researching

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: prynty <prynty@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/24 15:07:42 by prynty            #+#    #+#             */
-/*   Updated: 2024/08/27 14:22:17 by prynty           ###   ########.fr       */
+/*   Updated: 2024/08/28 15:13:52 by prynty           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
-static inline void  return_exit_code(t_pipex *pipex)
+static inline void	return_exit_code(t_pipex *pipex)
 {
     int exitcode;
     
@@ -20,9 +20,22 @@ static inline void  return_exit_code(t_pipex *pipex)
     exit (exitcode);
 }
 
+// void	parent_process(char **argv, int *pipe_fd, char **envp)
+// {
+// 	int	fd;
+
+// 	fd = open_file(argv[4], 1);
+// 	dup2(fd, 1);
+// 	dup2(pipe_fd[0], 0);
+// 	close(pipe_fd[1]);
+// 	//execute(pipex->argv[2], pipex->envp)
+// }
+
 int	main(int argc, char **argv, char **envp)
 {
 	t_pipex	pipex;
+	int		pipe_fd[2];
+	pid_t	pid;
 
 	pipex.argc = argc;
 	pipex.argv = argv;
@@ -33,5 +46,15 @@ int	main(int argc, char **argv, char **envp)
 		ft_printf("Usage: ./pipex infile cmd1 cmd2 outfile\n");
 		exit (1);
 	}
+	if (pipe(pipe_fd) == -1)
+	{
+		ft_putstr_fd("Pipe failed", 2);
+		exit (-1);
+	}
+	if (pid == -1)
+		exit (-1);
+	// if (!pid)
+	// 	child_process(pipex.argv, pipe_fd, pipex.envp);
+	// parent_process(pipex.argv, pipe_fd, pipex.envp);
 	return_exit_code(&pipex);
 }
